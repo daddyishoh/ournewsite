8c97ca80bf76e766ca5b65baac258ea85925df17
daddyishoh <50054033+daddyishoh@users.noreply.github.com>
2020-05-20 17:27:26 -0400

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+const loginName = document.querySelector("#loginName");
+const checkName = document.querySelector("#check");
+const loginAuth = document.querySelector(".loginAuth");
+const content = document.querySelector(".content");
+
+checkName.addEventListener("click", auth);
+
+function auth() {
+    if (loginName.value.toLowerCase() == "goodgirl") {
+        showPage();
+    } else{
+        return;
+    }
+}
+
+function showPage() {
+    loginAuth.style.display = "none";
+    content.style.display = "block";
+}
